9f1a3c6b35cec438703fd56a5ac2ad6f9f0fb2eb
brunotan14 <brunotan63@gmail.com>
2025-08-20 18:38:19 -0300

    Criação da pipeline e relatorio mocha

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'qn1yft',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     baseUrl: 'https://api.restful-api.dev',
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,3 +16,4 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import './page'
+import 'cypress-mochawesome-reporter/register';
